f56c094121337312c355eac6de35f90630bce608
Aitor <soy@aitor.me>
2020-04-11 18:17:18 +0200

    P4 test cases added

--- p4.py
+++ p4.py
@@ -33,6 +33,15 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
+# Default Udacity test cases:
+print("\nNormal cases:")
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
+# Edge cases added by me:
+print("\nEdge cases:")
+test_function([0, 1, 1, 0, 1])
+test_function([0, 0, 0])
+test_function([])
