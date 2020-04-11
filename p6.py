6df283c773fede44f048554b12d4dce634abfba1
Aitor <soy@aitor.me>
2020-04-11 17:53:45 +0200

    P6 test cases

--- p6.py
+++ p6.py
@@ -22,4 +22,14 @@ import random
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
+# Default Udacity test cases:
+print("\nNormal cases:")
 print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+
+
+# Edge cases added by me:
+print("\nEdge cases:")
+print ("Pass" if ((1, 7) == get_min_max([1,2,3,4,5,6,7])) else "Fail")
+print ("Pass" if ((0,0) == get_min_max([0])) else "Fail")
+print ("Pass" if ((-1,40) == get_min_max([-1,1,2,3,40])) else "Fail")
+print ("Pass" if ((-10,-1) == get_min_max([-10,-5,-1])) else "Fail")
