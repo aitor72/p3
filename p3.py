26a3dc0a77303aa6534cd4cf08f4f0042d515e4f
Aitor <soy@aitor.me>
2020-04-12 10:28:16 +0200

    P3 test cases added & readme updated

--- p3.py
+++ p3.py
@@ -28,5 +28,17 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
+# Default Udacity test cases:
+print("\nNormal cases:")
 test_function([[1, 2, 3, 4, 5], [542, 31]])
 test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function(test_case)
+
+
+# Edge cases added by me:
+print("\nEdge cases:")
+test_function([[1, 1], [1, 1]])
+test_function([[0, 1], [0, 1]])
+test_function([[0, 0], [0, 0]])
+test_function([[-5, 0], [-5, 0]])
